3e37a96d91accdeb3db81928f2bcedbed22996f7
ghanibaskara <ghanibaskaraa111@gmail.com>
2025-11-27 11:26:45 +0700

    Merge Chatbot

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('register_edvizo');
 });
 
 Route::get('/login-edvizo', function () {
@@ -22,3 +22,19 @@ Route::get('/register-edvizo', [RegisterController::class, 'show'])->name('regis
 
 // Proses Data Register (Disiapkan untuk nanti)
 Route::post('/register-edvizo/action', [RegisterController::class, 'process'])->name('register.action');
+
+use App\Http\Controllers\ChatbotController;
+
+Route::get('/konsultasi-jurusan', [ChatbotController::class, 'index'])->name('chatbot.index');
+Route::post('/konsultasi-jurusan/send', [ChatbotController::class, 'sendMessage'])->name('chatbot.send');
+
+use Illuminate\Support\Facades\Http;
+
+
+Route::get('/debug-gemini', function () {
+    $apiKey = env('GEMINI_API_KEY');
+    // Kita minta daftar model yang tersedia
+    $response = Http::withoutVerifying()->get("https://generativelanguage.googleapis.com/v1beta/models?key={$apiKey}");
+
+    return $response->json();
+});
